588b80fabecae4811ff317397d209ea716712a47
bashyrov <bashyroov@gmail.com>
2025-12-06 16:59:30 +0100

    Initialize Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,17 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.CharField(max_length=100, default="Test")  #TODO: Change to User model
+
+    def set_actual_return_date(self):
+        self.actual_return_date = date.today()
+        self.save()
+
