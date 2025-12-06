c83dd4579401995dc611cde600dd167b4c3ba3d0
bashyrov <bashyroov@gmail.com>
2025-12-06 17:42:21 +0100

    Add inventory validation to BorrowingSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -8,3 +8,11 @@ class BorrowingSerializer(serializers.ModelSerializer):
         fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user", )
         read_only_fields = ("user", "actual_return_date", "borrow_date")
 
+    def validate(self, data):
+        book = data["book"]
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError("We don't have enough inventory")
+
+        return data
+
