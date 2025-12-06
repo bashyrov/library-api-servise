01027773aff0a61c338b5e1caf59cf63b553b542
bashyrov <bashyroov@gmail.com>
2025-12-06 17:04:20 +0100

    Created BorrowingGeneric view

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingGeneric(generics.ListCreateAPIView):
+    queryset = Borrowing.objects.filter()
+    serializer_class = BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+        )
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if not instance.user == self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+
+        return queryset
+
