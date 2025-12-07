d7222adb5a436ba92aef669db700dd1e90ce8a43
bashyrov <bashyroov@gmail.com>
2025-12-07 17:35:25 +0100

    Added simple REST_FRAMEWORK settings

--- library_api_service/settings.py
+++ library_api_service/settings.py
@@ -123,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = 'users.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
