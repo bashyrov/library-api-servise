c0b0595c82bc099e935126fe4497d519cb588278
bashyrov <bashyroov@gmail.com>
2025-12-13 21:19:17 +0100

    Update url path for user creating

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from users import views
 app_name = 'user'
 
 urlpatterns = [
-    path('register/', views.CreateUserView.as_view(), name="create"),
+    path('', views.CreateUserView.as_view(), name="create"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
