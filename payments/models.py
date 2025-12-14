e4b6c28c460606001b6d3989cdbaa6847210cf19
bashyrov <bashyroov@gmail.com>
2025-12-14 17:38:51 +0100

    Created Payment model, add migrations

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+class Payment(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        PAID =  "Paid"
+
+    class Type(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
+
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices,
+        default=StatusChoices.PENDING,
+    )
+
+    type = models.CharField(
+        max_length=10,
+        choices=StatusChoices,
+    )
+
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.PROTECT)
+    session_url = models.URLField(null=False, blank=False)
+    session_id = models.CharField(null=False, blank=False, max_length=255)
+    money_to_paid = models.DecimalField(decimal_places=2, max_digits=10)
