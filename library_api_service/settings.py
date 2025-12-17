3fbe4011bef56c30823ed8015c41a7c1dd7ff41e
bashyrov <bashyroov@gmail.com>
2025-12-18 00:01:46 +0100

    Add tg_notifications app

--- library_api_service/settings.py
+++ library_api_service/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     "users",
     "rest_framework",
     "rest_framework_simplejwt",
-    "payments"
+    "payments",
+    "tg_notifications"
 ]
 
 MIDDLEWARE = [
---
+++ tg_notifications/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TgNotificationsConfig(AppConfig):
+    name = 'tg_notifications'
