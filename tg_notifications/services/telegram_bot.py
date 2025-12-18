8a870ad45553de67b6a6328d3e1b78a92eff11d5
bashyrov <bashyroov@gmail.com>
2025-12-18 02:09:18 +0100

    Updating send_message with using request and tg rpc

--- tg_notifications/services/telegram_bot.py
+++ tg_notifications/services/telegram_bot.py
@@ -1,9 +1,15 @@
+import requests
 from telegram import Bot
 from django.conf import settings
 
 bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
 
 
-def send_message(text):
-    for chat_id in settings.ADMIN_TELEGRAM_CHAT_IDS:
-        bot.send_message(chat_id=chat_id, text=text)
+def send_message(text: str):
+    url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
+    chat_id = settings.TELEGRAM_CHAT_ID
+    try:
+        response = requests.post(url, json={"chat_id": chat_id, "text": text}, timeout=5)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Failed to send message to {chat_id}: {e}")
